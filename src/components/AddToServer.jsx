a599d90c586c2842dc64e21cdbd1676fe7691308
Mac Sweeny <macsweenydev@gmail.com>
2022-10-16 11:41:21 +1000

    Added Glow and changed port

--- src/components/AddToServer.jsx
+++ src/components/AddToServer.jsx
@@ -3,7 +3,7 @@ import { arrowUp } from "../assets";
 
 const AddToServer = () => (
   <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-purple-gradient p-[2px] cursor-pointer`}>
-    <div className={`${styles.flexCenter} flex-col bg-black w-[100%] h-[100%] rounded-full`}>
+    <div className={`${styles.flexCenter} flex-col bg-black w-[100%] h-[100%] glow-on-hover rounded-full`}>
       <div className={`${styles.flexStart} flex-row`}>
         <p className="font-poppins font-medium text-[18px] leading-[23.4px]">
           <span className="text-white">Add to</span>
