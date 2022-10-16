e2bce57940b20144587b936a7f358adad8945157
Mac Sweeny <macsweenydev@gmail.com>
2022-10-16 17:10:55 +1000

    Fixed Animation Bug

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -47,16 +47,15 @@ const Hero = () => {
         </FadeInSection>
       </div>
 
+      <FadeInSection>
       <div
         className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}
       >
-        <FadeInSection>
         <img
           src={robot}
           alt="EarnRewards"
           className="w-[100%] h-[100%] scale-110 relative z-[5]"
         />
-        </FadeInSection>
 
         {/* gradient start */}
         <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
@@ -64,6 +63,7 @@ const Hero = () => {
         <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
         {/* gradient end */}
       </div>
+      </FadeInSection>
 
       <div className={`ss:hidden ${styles.flexCenter}`}>
         <AddToServer />
